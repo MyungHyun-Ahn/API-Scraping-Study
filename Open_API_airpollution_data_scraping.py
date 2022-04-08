d287c80e801c2c8baf8a1686ccbf482a1f785934
MyungHyun-Ahn <ahn020367@gmail.com>
2022-04-08 22:23:42 +0900

    TM 좌표 기준으로 근접 측정소 목록 조회

--- Open_API_airpollution_data_scraping.py
+++ Open_API_airpollution_data_scraping.py
@@ -4,12 +4,44 @@
 #(https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15073877)
 
 import requests
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus
+import pandas as pd
+
+from xml.etree import ElementTree
+
+
+# .gitignore 파일에 추가하여 내 컴퓨터에서만 볼수있음.
 from secretkey import key
 
-url = 'http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getMsrstnList'
+decode_key = requests.utils.unquote(key)
 
-params ={'serviceKey' : key, 'returnType' : 'xml', 'numOfRows' : '100', 'pageNo' : '1', 'addr' : '서울', 'stationName' : '종로구' }
+url = 'http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList'
 
-response = requests.get(url, params=params)
-print(url)
-print(response.content)
+params2 = '?' + 'ServiceKey=' + key + '&' + \
+urlencode({ quote_plus('returnType') : 'xml',
+            quote_plus('tmX') : '244148.546388',
+            quote_plus('tmY') : '412423.75772',
+            quote_plus('ver') : '1.0' })
+print(url + params2)
+
+request = Request(url + params2)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read()
+
+print(response_body)
+
+root = ElementTree.fromstring(response_body)
+
+df = pd.DataFrame()
+
+for item in root.iter('item'):
+    
+    item_dict = {}
+    item_dict['tm'] = item.find('tm').text
+    item_dict['stationName'] = item.find('stationName').text
+    item_dict['addr'] = item.find('addr').text
+
+    df = df.append(item_dict, ignore_index=True)
+
+print(df)
